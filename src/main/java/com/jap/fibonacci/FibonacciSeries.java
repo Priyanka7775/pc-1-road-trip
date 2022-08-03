6753519ad442a7326a0336e3a4e6aa5a3958013d
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-03 14:28:14 +0530

    Sprint 4

--- src/main/java/com/jap/fibonacci/FibonacciSeries.java
+++ src/main/java/com/jap/fibonacci/FibonacciSeries.java
@@ -2,18 +2,37 @@ package com.jap.fibonacci;
 
 public class FibonacciSeries
 {
+    private long fibonacci;
+
     // Write the logic in the method to generate the next number in the Fibonacci series
+
     public long fibonacci(long number) {
-        return -2;
+        return number-2;
     }
     // Print the series inside the method
-    public void printFibonacciSeries(long number){
+    static long n1=0,n2=1,n3;
+    public long printFibonacciSeries(long number){
 
+       if(number>0) {
+           n3 = n1 + n2;
+           n1 = n2;
+           n2 = n3;
+           System.out.print(" " + n3);
+           printFibonacciSeries(number-1);
+       }
+       return number;
     }
     public static void main( String[] args )
     {
+
+        long number=10;
+
         FibonacciSeries fib = new FibonacciSeries();
-        // Call the  printFibonacciSeries() method and pass the value.
+
+        fib.printFibonacciSeries(number);
+
+        //System.out.println(n1+" "+n2);
+        //printFibonacciSeries(number-2);
 
 
     }
